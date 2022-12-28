5012d062618d8a6f6ebf9e050e8ea30b6921547a
Francisco Lercari <franciscolercari@gmail.com>
2022-12-27 21:58:54 -0500

    [FIX] test model

--- app/core/models.py
+++ app/core/models.py
@@ -11,9 +11,9 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(self, email, password=None, **extra_fileds):
-        """Create, sabe and return a new user."""
-        user = self.model(email=email, **extra_fileds)
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
